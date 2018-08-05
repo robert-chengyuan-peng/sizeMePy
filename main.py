9815f15fce76934bdb98caa66985c79c21f3c7f9
Robert <robertpengcode@gmail.com>
2018-08-05 02:27:04 -0400

    adding user login functionality

--- main.py
+++ main.py
@@ -34,8 +34,20 @@ class MainPage(webapp2.RequestHandler):
 	#mainpage req handler
 
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello')
+		user = users.get_current_user()
+		if user:
+			nickname = user.nickname()
+			logout_url = users.create_logout_url('/')
+			greeting = 'Hello, {}! (<a href="{}"> sign out</a>)'.format(nickname, logout_url)
+		else:
+			login_url = users.create_login_url('/')
+			greeting = '<a href="{}"> sign in</a>'.format(login_url)
+
+		self.response.write('<html><body>{}</body></html>'.format(greeting))
+
+
+		# self.response.headers['Content-Type'] = 'text/plain'
+		# self.response.write('Hello')
 		logging.info('test')
 
 
